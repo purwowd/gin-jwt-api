e7342e4cc913671cb849d7a2304a486819762aee
purwowd <id.purwowd@gmail.com>
2020-11-26 12:13:03 +0700

    serve static image

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 
 	router := gin.Default()
 	router.Use(cors.Default())
+	router.Static("images", "./images")
 
 	api := router.Group("/api/v1")
 	api.POST("/users/register", userHandler.RegisterUser)
